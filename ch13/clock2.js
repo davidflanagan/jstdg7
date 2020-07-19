b34b08d3b58bf0d78b70295473c2b8174b14bac3
David Flanagan <dflanagan@mozilla.com>
2020-07-18 20:38:45 -0700

    Fix erratum on page 373

--- ch13/clock2.js
+++ ch13/clock2.js
@@ -5,20 +5,21 @@ function clock(interval, max=Infinity) {
         return new Promise(resolve => setTimeout(resolve, time - Date.now()));
     }
 
+    let startTime = Date.now();  // Remember when we started
+    let count = 0;               // Remember which iteration we're on
+
     // Return an asynchronously iterable object
     return {
-        startTime: Date.now(),  // Remember when we started
-        count: 1,               // Remember which iteration we're on
-        async next() {          // The next() method makes this an iterator
-            if (this.count > max) {     // Are we done?
+        async next() {           // The next() method makes this an iterator
+            if (++count > max) { // Are we done?
                 return { done: true };  // Iteration result indicating done
             }
             // Figure out when the next iteration should begin,
-            let targetTime = this.startTime + this.count * interval;
+            let targetTime = startTime + count * interval;
             // wait until that time,
             await until(targetTime);
             // and return the count value in an iteration result object.
-            return { value: this.count++ };
+            return { value: count };
         },
         // This method means that this iterator object is also an iterable.
         [Symbol.asyncIterator]() { return this; }
